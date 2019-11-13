591a6f087f05cda7900231eef0609382c6d33c51
Técnico em Informática 2019.2 <42121742019.2@senacrjedu.local>
2019-11-13 20:13:55 -0300

    13112019

--- src/app/model/usuario.ts
+++ src/app/model/usuario.ts
@@ -1,7 +1,10 @@
+import { Endereco } from './endereco';
+
 export class Usuario {
     nome:string;
     email:string;
     pws:string;
     foto:string;
     ativo:boolean = true;
+    enderecos:Endereco[] = []
 }
