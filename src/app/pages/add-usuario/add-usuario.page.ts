7ae21b37ec1fd2a41e70e0b08c75130a8ec54b7b
Tecnico em Informatica 2019.1 <16121742019.1@senacrjedu.local>
2019-11-22 21:47:01 -0300

    13112019

--- src/app/pages/add-usuario/add-usuario.page.ts
+++ src/app/pages/add-usuario/add-usuario.page.ts
@@ -3,6 +3,8 @@ import { Usuario } from 'src/app/model/usuario';
 import { Router } from '@angular/router';
 import { UsuarioService } from 'src/app/services/usuario.service';
 import { MensagemService } from 'src/app/services/mensagem.service';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-add-usuario',
@@ -16,7 +18,9 @@ export class AddUsuarioPage implements OnInit {
   constructor(
     private usuarioService: UsuarioService,
     private msg: MensagemService,
-    private router:Router
+    private router: Router,
+    private camera: Camera,
+    public actionSheetController: ActionSheetController
   ) { }
 
   ngOnInit() {
@@ -34,8 +38,84 @@ export class AddUsuarioPage implements OnInit {
       },
       erro => {
         console.log("Erro: ", erro);
-        this.msg.presentAlert("Ops!", "Erro ao tentar cadastrar!\nVerique os dados ou se o e-mail já foi cadastrado!" );
+        this.msg.presentAlert("Ops!", "Erro ao tentar cadastrar!\nVerique os dados ou se o e-mail já foi cadastrado!");
       }
     )
   }
+
+  tirarFoto() {
+    const options: CameraOptions = {
+      quality: 50,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      this.usuario.foto = base64Image;
+    }, (err) => {
+      // Handle error
+    });
+  }
+
+  pegarFoto() {
+    const options: CameraOptions = {
+      quality: 50,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      this.usuario.foto = base64Image;
+    }, (err) => {
+      // Handle error
+    });
+  }
+
+  async escolherFoto() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Escolhar Opção',
+      buttons: [
+        {
+          text: 'Camera',
+          icon: 'camera',
+          handler: () => {
+            this.tirarFoto()
+          }
+        },
+        {
+          text: 'Galeria',
+          icon: 'photos',
+          handler: () => {
+            this.pegarFoto()
+          }
+        },
+        {
+          text: 'Remover Foto',
+          icon: 'qr-scanner',
+          handler: () => {
+            this.usuario.foto = null;
+          }
+        },
+        {
+          text: 'Cancelar',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }]
+    });
+    await actionSheet.present();
+  }
+
+
 }
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { MensagemService } from 'src/app/services/mensagem.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  protected email:string = null;
+  protected pws:string = null;
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private router:Router,
+    private msg:MensagemService
+  ) { }
 
   ngOnInit() {
   }
+  
+  onSubmit(fc){
+    this.login()
+  }
+
+  login(){
+    this.afAuth.auth.signInWithEmailAndPassword(this.email, this.pws).then(
+      res=>{
+        this.router.navigate([''])
+      },
+      err=>{
+        console.log(err);
+        this.msg.presentAlert("Ops!", "Não foi encotrado o usuario!");
+      }
+    )
+  }
+
+
 
 }
